a0afc8eb16898c548d412c8455dbe4c7c8a73d7d
Cody Robertson <codyallanrobertson@gmail.com>
2019-12-20 14:08:05 -0600

    Repair virtual environment

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/codyrobertson/PycharmProjects/AdventOfCode2019/venv"
+set -gx VIRTUAL_ENV "/Users/cody.robertson/Dev/advent-of-code-2019/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,7 +52,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(venv) "            
+        if test -n "(venv) "
             printf "%s%s" "(venv) " (set_color normal)
         else
             # ...Otherwise, prepend env
